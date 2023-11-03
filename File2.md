5368fcebef140569b74139512f0965b9ec5171c9
weykon <635883092@qq.com>
2023-11-03 12:56:31 +0800

    update

--- File2.md
+++ File2.md
@@ -2,3 +2,4 @@ shift + O create new line to before paraea
 there we go .
 
 I want to try the terminal in this editor
+show me the copilot please.   
--- test_terminal.js
+++ test_terminal.js
@@ -1,2 +1,8 @@
-
 console.log("it is running ...")
+
+// 我这里开始去学习nvim对于的lua的相关
+
+// vim 这里有三层代码级别的区分
+// 1. vimscript
+// 2. nvim的api
+// 3. lua的，有一个lua stdlib提供在里面
