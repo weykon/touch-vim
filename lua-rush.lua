8a1eba5fcdc4230af9907186a6d052dd50d21ec7
weykon <635883092@qq.com>
2023-11-05 05:41:13 +0800

    update

--- lua-rush.lua
+++ lua-rush.lua
@@ -1,4 +1,8 @@
 -- :lua 这样就可以在cmd中像python一样运行lua代码了。
 -- 那么现在这个文件，我跟着教程，应该是以文件形式来记录代码。
 
- 
+local a = 1
+print(a)
+
+vim.g.my_first_global_var = 1
+vim.print(vim.g.my_first_global_var)
---
+++ star_pressing.rs
@@ -0,0 +1,9 @@
+fn main (){
+    let a = 32; 
+    let b = 32;
+}
+
+// here is to test the * while the cursor at the starting on word
+// let 
+// ^
+// then press &, it will put it into search input
